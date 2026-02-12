fcb00e48017c401742e659784c4b954b6506dfe5
gulshan-2003 <gulshantagore2003@gmail.com>
2026-02-12 22:42:26 +0530

    This is the perfect solution of Finding Second Largest Element ina an Array

---
+++ src/Array/SecondLargest.java
@@ -0,0 +1,34 @@
+package Array;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int secondLargest = findSecondLargest(arr);
+        System.out.println("The second largest element is: " + secondLargest);
+    }
+
+    public static int findSecondLargest(int[] arr) {
+        if (arr.length < 2) {
+            throw new IllegalArgumentException("Array must have at least two elements");
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException("There is no second largest element");
+        }
+
+        return secondLargest;
+    }
+    
+}
